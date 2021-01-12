ee03323686f4211f9e8fd9bb67aa63426dbd9fd8
Jonathan  Tran <110579@eesd.org>
2021-01-12 00:08:25 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# This is a comment, comments are not run by the compiler
+# Output: Information that the computer prints out, what you see printed on the right hand side
+print("Hello World.")
+print("My name is Jonathan Tran.")
+print("I don't have a favorite book, but I enjoyed reading the 'Harry Potter' Series.")
+print("I don't have a favorite superhero.")
+
+print("Hello")
+print("World")
+
+#Concatenation : add two strings together using '+' 
+
+print("Hello" + "World")
